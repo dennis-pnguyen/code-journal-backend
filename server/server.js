ff34a6e03e32e519946060729888998bf9921d2e
jessicagrayson <grayjessica12@hotmail.com>
2023-09-20 17:29:46 +0000

    added functionality to retrieve entry by id

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ const db = new pg.Pool({
 const app = express();
 app.use(express.json());
 
+// Get array of all entries
 app.get('/api/entries', async (req, res, next) => {
   try {
     const sql = `SELECT *
@@ -21,6 +22,31 @@ app.get('/api/entries', async (req, res, next) => {
     const params = [];
     const result = await db.query(sql, params);
     const entries = result.rows;
+    res.status(200).json(entries);
+  } catch (err) {
+    console.error(err);
+    next();
+  }
+});
+
+// Get entry by entryId
+app.get('/api/entries/:entryId', async (req, res, next) => {
+  try {
+    const entryId = Number(req.params.entryId);
+    if (!Number.isInteger(entryId) || entryId <= 0) {
+      throw new ClientError(400, '"entryId" must be an integer');
+    }
+    const sql = `SELECT *
+    from "entries"
+    where "entryId" = $1
+    `;
+    const params = [entryId];
+    const result = await db.query(sql, params);
+    const entries = result.rows[0];
+
+    if (!entries) {
+      throw new ClientError(404, `Cannot find entry with ${entryId}`);
+    }
     res.status(200).json(entries);
   } catch (err) {
     console.error(err);
