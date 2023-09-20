d4112a6ecc4b7929cbc2a90d7d1bd6f233f8a806
Dennis Nguyen <dennis.nguyen9925@gmail.com>
2023-09-20 17:14:25 +0000

    Added GET endpoint to server.js.

--- server/server.js
+++ server/server.js
@@ -14,6 +14,20 @@ const db = new pg.Pool({
 const app = express();
 app.use(express.json());
 
+app.get('/api/entries', async (req, res, next) => {
+  try {
+    const sql = `SELECT *
+    from "entries"`;
+    const params = [];
+    const result = await db.query(sql, params);
+    const entries = result.rows;
+    res.status(200).json(entries);
+  } catch (err) {
+    console.error(err);
+    next();
+  }
+});
+
 app.use(errorMiddleware);
 app.listen(process.env.PORT, () => {
   console.log(`express server listening on port ${process.env.PORT}`);
